f33baca3580134cfd798f3a29098ce5356876d62
janhvii21 <janavipratihast00@gmail.com>
2026-07-01 07:56:12 +0545

    feat: add JWT route protection for movies endpoints

--- backend/app/api/movies.py
+++ backend/app/api/movies.py
@@ -2,19 +2,29 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List, Optional
 from app.core.database import get_db
+from app.core.security import get_current_user
 from app.schemas.movie import MovieResponse
 from app.services.movie_service import get_all_movies, get_movie_by_id
+from app.models.models import User
 
 router = APIRouter()
 
 
 @router.get("/movies", response_model=List[MovieResponse])
-def list_movies(search: Optional[str] = None, db: Session = Depends(get_db)):
+def list_movies(
+    search: Optional[str] = None,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)  # 👈 protected
+):
     return get_all_movies(db, search)
 
 
 @router.get("/movies/{movie_id}", response_model=MovieResponse)
-def get_movie(movie_id: int, db: Session = Depends(get_db)):
+def get_movie(
+    movie_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)  # 👈 protected
+):
     movie = get_movie_by_id(db, movie_id)
     if movie is None:
         raise HTTPException(status_code=404, detail="Movie not found")
--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -2,6 +2,10 @@ from passlib.context import CryptContext
 from jose import jwt
 from datetime import datetime, timedelta
 from app.core.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from app.core.database import get_db
 
 # This tool turns plain passwords into scrambled, unreadable text
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -20,3 +24,22 @@ def create_access_token(data: dict) -> str:
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = payload.get("user_id")
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+    except Exception:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+
+    from app.models.models import User
+    user = db.query(User).filter(User.user_id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not found")
+    return user
